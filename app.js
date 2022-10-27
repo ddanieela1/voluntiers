b00f8661d08ac6c770f18a71487315d0394fbf0f
CatW-M <wheelermushlin@gmail.com>
2022-10-26 23:16:33 -0600

    fixed passport

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+// Imports
+require('dotenv').config()
 const express = require('express');
-
 const cors = require('cors');
+const passport = require('passport');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const flash = require('connect-flash');
 require('./config/passport')(passport);
 
-//App Set Up
+// App Set up
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-//Middleware
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); // JSON parsing
 app.use(cors()); // allow all CORS requests
 app.use(passport.initialize());
-//Database Set Up
+
+// Database Set Up
 const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
 mongoose.connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
@@ -27,48 +27,18 @@ db.once('open', () => {
 
 db.on('error', (error) => {
     console.log(`Database Error: ${error}`);
-});
-
-
-// SuperHero.create({
-//     realName: 'Clark Kent',
-//     alterEgo: 'Superman',
-//     company: 'Marvel'
-// })
-// .then(superhero => {
-//     console.log('NEW HERO', superhero);
-// })
-// .catch(error => {
-//     console.log('ERROR', error);
-// })
-
-//API Routes
-
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to Voluntiers' });
-});
-
-// ================ SUPERHEROS ROUTES ========================
-// app.use('/superheros', require('./controllers/superheros'));
-
-// ================ CREDIT CARDS ROUTES ========================
-// app.use('/credits', require('./controllers/credits'));
-
-// ================ VEHICLES ROUTES ========================
-// app.use('/vehicles', require('./controllers/vehicles'));
-
+})
 
 // API Routes
 app.get('/', (req, res) => {
-    res.json({ name: 'MERN Auth API', greeting: 'Welcome to the our API', author: 'YOU', message: "Smile, you are being watched by the Backend Engineering Team" });
-  });
-  
-  app.use('/examples', require('./controllers/example'));
-  app.use('/users', require('./controllers/user'));
-  
-  // Server
-  const server = app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
-  
-  module.exports = server;
+  res.json({ name: 'MERN Auth API', greeting: 'Welcome to the our API', author: 'YOU', message: "Smile, you are being watched by the Backend Engineering Team" });
+});
+
+app.use('/examples', require('./controllers/example'));
+app.use('/users', require('./controllers/user'));
+
+// Server
+const server = app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+
+module.exports = server;
   
