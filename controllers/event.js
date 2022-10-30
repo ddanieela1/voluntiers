368e96c2c3b3918788d30c64ed7b5efecf9f0557
ddanieela1 <vdaniela.gv@gmail.com>
2022-10-29 20:39:00 -0400

    updated put route for event

--- controllers/event.js
+++ controllers/event.js
@@ -1,104 +1,90 @@
 // Imports
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 const { JWT_SECRET } = process.env;
 
 // DB Models
-const Event = require('../models/event');
+const Event = require("../models/event");
 
-
-router.get('/', (req, res) => {
-    Event.find({})
-    .then(events => {
-        console.log('All events', events);
-        res.json({ events: events });
+router.get("/", (req, res) => {
+  Event.find({})
+    .then((events) => {
+      console.log("All events", events);
+      res.json({ events: events });
     })
-    .catch(error => { 
-        console.log('error', error)
-        res.json({ message: 'Error occured, please try again' });
+    .catch((error) => {
+      console.log("error", error);
+      res.json({ message: "Error occured, please try again" });
     });
 });
 
-router.get('/:id', (req, res) => {
-    console.log('find events by', req.params.id)
-    Event.findOne({
-        id: req.params.id
+router.get("/:id", (req, res) => {
+  console.log("find events by", req.params.id);
+  Event.findOne({
+    id: req.params.id,
+  })
+    .then((events) => {
+      console.log("Here is the event", events.id);
+      res.json({ events: events });
     })
-    .then(events => {
-        console.log('Here is the event', events.id);
-        res.json({ events: events });
-    })
-    .catch(error => { 
-        console.log('error', error);
-        res.json({ message: "Error ocurred, please try again" });
+    .catch((error) => {
+      console.log("error", error);
+      res.json({ message: "Error ocurred, please try again" });
     });
 });
 
-router.post('/', (req, res) => {
-    Event.create({
-        name: req.body.name,
-        date: req.body.date,
-        location: req.body.location,
-        startTime: req.body.time,
-        endTime: req.body.endTime,
-        users: req.body.users,
-        organizationId: req.body.organizationId,
+router.post("/", (req, res) => {
+  Event.create({
+    name: req.body.name,
+    date: req.body.date,
+    location: req.body.location,
+    startTime: req.body.time,
+    endTime: req.body.endTime,
+    users: req.body.users,
+    organizationId: req.body.organizationId,
+  })
+    .then((events) => {
+      console.log("New event =>>", events);
+      res.json({ events: events });
     })
-    .then(events=> {
-        console.log('New event =>>', events);
-        res.json({ events: events });
-    })
-    .catch(error => { 
-        console.log('error', error) 
-        res.json({ message: "Error ocurred, please try again" })
+    .catch((error) => {
+      console.log("error", error);
+      res.json({ message: "Error ocurred, please try again" });
     });
 });
 
-router.put('/:id', (req, res) => {
-  
-    Event.findById(req.params.id )
-    .then(foundEvent=> {
-        console.log('Event found', foundEvent);
-        Event.findOneAndUpdate({ id: req.params.id}, { 
-        name: req.body.name,
-        date: req.body.date,
-        location: req.body.location,
-        startTime: req.body.time,
-        endTime: req.body.endTime,
-        users: req.body.users,
-        organizationId: req.body.organizationId,
-        }, { 
-            upsert: true 
-        })
-        .then(events => {
-            console.log('Event was updated', events);
-            res.json({ events: events })
-        })
-        .catch(error => {
-            console.log('error', error) 
-            res.json({ message: "Error ocurred, please try again" })
-        })
-    })
-    .catch(error => {
-        console.log('error', error) 
-        res.json({ message: "Error ocurred, please try again" })
-    })
+router.put("/:id", async(req, res) => {
+    try {
+        const data = await Event.findById(req.params.id);
+        res.json({ data: data });
+    } catch (error) {
+    console.log(error);
+    }
+//   Event.findById(req.params.id)
+//     .then((error, foundEvent) => {
+//       console.log("Event found", foundEvent);
+//       res.json({ foundEvent: foundEvent });
+//     })
+//     .catch((error) => {
+//       console.log("error", error);
+//       res.json({ message: "Error ocurred, please try again" });
+//     });
 });
 
-router.delete('/:id', (req, res) => {
-    Event.findOneAndRemove({ id: req.params.id})
-    .then(response => {
-        console.log('This was deleted', response);
-        res.json({ message: `${req.params.id} was deleted`});
+router.delete("/:id", (req, res) => {
+  Event.findOneAndRemove({ id: req.params.id })
+    .then((response) => {
+      console.log("This was deleted", response);
+      res.json({ message: `${req.params.id} was deleted` });
     })
-    .catch(error => {
-        console.log('error', error) 
-        res.json({ message: "Error ocurred, please try again" });
-    })
+    .catch((error) => {
+      console.log("error", error);
+      res.json({ message: "Error ocurred, please try again" });
+    });
 });
 
 module.exports = router;
